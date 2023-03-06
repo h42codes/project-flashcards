2b5c6d1d33509e9d8ac7c85e37c6e07905c71f49
h42codes <99951343+h42codes@users.noreply.github.com>
2023-03-07 02:22:34 +0900

    Implement random indexing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Flashcard from "./components/Flashcard";
+import triviaData from "./data/triviaData.json";
+// import triviaData from "./data/triviaData.jsx";
 
 function App() {
+  const [cardIndex, setCardIndex] = useState(0);
+
+  const getRandomIndex = () => {
+    let nextRandomIndex;
+    do {
+      nextRandomIndex = Math.floor(Math.random() * triviaData.length);
+    } while (nextRandomIndex === cardIndex);
+    setCardIndex(nextRandomIndex);
+    return nextRandomIndex;
+  };
+
+  const handleNextClick = () => {
+    setCardIndex(getRandomIndex());
+  };
+
   return (
     <div className="App">
       <h1>⚡️ Harry Potter Trivia ⚡️</h1>
       <h3>How big of a fan are you? Test your knowledge!</h3>
-      <p>Number of cards: </p>
+      <p>Number of cards: {triviaData.length}</p>
       {/* <p>Cards marked as pass</p> */}
-
-      <Flashcard />
-
-      <button>⭢</button>
+      <Flashcard data={triviaData[cardIndex]} />
+      <button onClick={handleNextClick}>⭢</button>
       {/* <footer>
         Photo by{" "}
         <a href="https://unsplash.com/@bitu2104?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
